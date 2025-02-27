1d4db4b4e8a10ddc6632ef749640d76acc0deac6
221B496 <221B496@juet.co.in>
2025-02-27 14:35:28 +0530

    deleted

--- LE4/Eager/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    Singleton S1=Singleton.getInstance();
-    public static void main(String[] args)
-    {
-        System.out.print("Design Pattern with singleton class");
-    }
-    
-}
