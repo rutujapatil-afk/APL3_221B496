6d29ac3b64351b0e5fe735916ce5622a75a39d00
221B496 <221B496@juet.co.in>
2025-05-01 15:10:34 +0530

    LE8

---
+++ LE8/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("caving");
+    }
+    public void fly(){
+        System.out.println("Flying high hover mountain");
+    }
+    public void eat(){
+        System.out.println("Eating Pearls");
+    }
+}
